7bdcef5026ff290c93327da80279d95dbf55e05c
TipDevs <tipdevs@gmail.com>
2025-08-19 04:12:51 +0100

    Add a function which does fibonacci operation recursively

--- fibonacci.js
+++ fibonacci.js
@@ -14,3 +14,17 @@ function fibs(n) {
 // assignment testing
 console.log("This was printed iteratively");
 console.log(fibs(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+
+// recursive fibonacci assignment solution
+function fibsRec(n) {
+  let arr = [];
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  arr = fibsRec(n - 1);
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return arr;
+}
+// assignment testing
+console.log("This was printed recursively");
+console.log(fibs(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
